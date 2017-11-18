5dbeab0c0c93d8d0c6653e2b93ee754c42df43cf
cogiaphuc97 <cogiaphuc97@gmail.com>
2017-11-19 00:21:27 +0700

    http and interfaces

---
+++ http/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "net/http"
+import "fmt"
+import "os"
+
+func main() {
+	resp, err := http.Get("https://www.google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	fmt.Println(resp)
+}
--- interfaces/main.go
+++ interfaces/main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+type bot interface {
+	getGreeting() string
+}
 type englishBot struct{}
 type spanishBot struct{}
 
@@ -11,16 +14,12 @@ func main() {
 	eb := englishBot{}
 	sb := spanishBot{}
 	printGreeting(eb)
-	// printGreeting(sb)
+	printGreeting(sb)
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
-
-// func printGreeting(sb spanishBot) {
-// 	fmt.Println(sb.getGreeting)
-// }
 
 func (englishBot) getGreeting() string {
 	return "Hello!"
